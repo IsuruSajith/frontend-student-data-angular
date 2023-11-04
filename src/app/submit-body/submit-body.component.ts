d39422eeb1bb18ca93fdba2fc942914eb63f7310
Isuru <isurusajith0@gmail.com>
2023-11-04 09:55:27 +0530

    update few things

--- src/app/submit-body/submit-body.component.ts
+++ src/app/submit-body/submit-body.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./submit-body.component.scss']
 })
 export class SubmitBodyComponent {
+  formData: any;
+  constructor() {}
 
+  submitForm() {
+// Log the collected data to the console
+    console.log('User data to send to the backend:', this.formData);
+
+    // TODO: Send the data to the backend using an HTTP request
+  }
 }
