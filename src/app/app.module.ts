e8a8cc24d903fed7bf4c0eaf5e16f4f06adbf8c4
Isuru <isurusajith0@gmail.com>
2023-11-04 09:36:04 +0530

    created new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SubmitBodyComponent } from './submit-body/submit-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubmitBodyComponent
   ],
   imports: [
     BrowserModule
