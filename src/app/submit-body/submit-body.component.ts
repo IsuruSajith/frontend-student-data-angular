ebce74f2a8ee56bf2e2a18104721171fa945eaec
Isuru <isurusajith0@gmail.com>
2023-11-04 09:40:31 +0530

    initiate

---
+++ src/app/submit-body/submit-body.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-body',
+  templateUrl: './submit-body.component.html',
+  styleUrls: ['./submit-body.component.scss']
+})
+export class SubmitBodyComponent {
+
+}
